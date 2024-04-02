0cbc3f1342ffbb5f44d325b202bc5c787f7a1715
Jimmy Helm <jimmyatthehelm@gmail.com>
2024-04-02 13:40:45 -0400

    objects lab

---
+++ app.js
@@ -0,0 +1,562 @@
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//   }
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+Solve Exercise 3 here:
+*/
+
+
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+
+
+
+
+
+// console.log(game)
+// //   console.dir(pokemon, { maxArrayLength: null })
+
+
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+// if (Pikachu) {
+//     game.party.push(Pikachu);
+// }
+
+// console.log(game)
+
+
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+// if (Pikachu) {
+//     game.party.push(Pikachu);
+// }
+
+//   const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+// if (Nidoking) {
+//   game.party.push(Nidoking)
+// }
+
+//   const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+// if (Jigglypuff) {
+//   game.party.push(Jigglypuff)
+// }
+
+//   const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+// if (Charizard) {
+//   game.party.push(Charizard) 
+// }
+
+// console.log(game)
+
+/*
+Exercise 6
+1. Arrange the Pokémon in `game.party` by their HP. The one with the highest HP should come first.
+2. You'll need to use the `.sort()` method. How does the compare function work in sorting numbers?
+
+
+Solve Exercise 6 here:
+*/
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+// if (Pikachu) {
+//     game.party.push(Pikachu);
+// }
+
+//   const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+// if (Nidoking) {
+//   game.party.push(Nidoking)
+// }
+
+//   const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+// if (Jigglypuff) {
+//   game.party.push(Jigglypuff)
+// }
+
+//   const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+// if (Charizard) {
+//   game.party.push(Charizard) 
+// }
+
+// game.party.sort((a, b) => b.hp - a.hp);
+
+// console.log(game)
+
+
+/*
+Exercise 7
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 7 here:
+*/
+
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+// if (Pikachu) {
+//     game.party.push(Pikachu);
+// }
+
+//   const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+// if (Nidoking) {
+//   game.party.push(Nidoking)
+// }
+
+//   const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+// if (Jigglypuff) {
+//   game.party.push(Jigglypuff)
+// }
+
+//   const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+// if (Charizard) {
+//   game.party.push(Charizard) 
+// }
+
+// game.gyms.forEach(gym => {
+//   if (gym.difficulty < 3) {
+//       gym.completed = true;
+//   }
+// });
+  
+
+// game.party.sort((a, b) => b.hp - a.hp);
+
+// console.log(game)
+
+
+/*
+Exercise 8
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. Remember that you're working with an array of objects - what array method is ideal for replacing one element with another? 
+
+Solve Exercise 8 here:
+*/
+
+
+// const pokemon = require('./data.js')
+
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+// if (Pikachu) {
+//     game.party.push(Pikachu);
+// }
+
+//   const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+// if (Nidoking) {
+//   game.party.push(Nidoking)
+// }
+
+//   const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+// if (Jigglypuff) {
+//   game.party.push(Jigglypuff)
+// }
+
+//   const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+// if (Charizard) {
+//   game.party.push(Charizard) 
+// }
+
+// game.gyms.forEach(gym => {
+//   if (gym.difficulty < 3) {
+//       gym.completed = true;
+//   }
+// });
+  
+
+// game.party.sort((a, b) => b.hp - a.hp);
+
+// for(let i = 0; i < game.party.length; i++) {
+//   for(let j = 0; j < pokemon.length; j++) {
+//     if (pokemon[j].name === game.party[i].name) {
+//       // console.log('Evolving Pokemon...', game.party[i].name)
+//       // console.log('new Poke!', pokemon[j + 1])
+//       // game.party.splice(i, 1, pokemon[j + 1])
+    
+//     }
+//   }
+// }
+
+// console.log("your new Pokemon!", game.party)
+// console.log(game.party)
+
+/*
+Exercise 9
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 9 here:
+*/
+
+
+// const pokemon = require('./data.js')
+
+//  const game = {
+//      party: [],
+//      gyms: [
+//        { location: "Pewter City", completed: false, difficulty: 1 },
+//        { location: "Cerulean City", completed: false, difficulty: 2 },
+//        { location: "Vermilion City", completed: false, difficulty: 3 },
+//        { location: "Celadon City", completed: false, difficulty: 4 },
+//        { location: "Fuchsia City", completed: false, difficulty: 5 },
+//        { location: "Saffron City", completed: false, difficulty: 6 },
+//        { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//        { location: "Viridian City", completed: false, difficulty: 8 },
+//      ],
+//     items: [
+//        { name: "potion", quantity: 4 },
+//        { name: "pokeball", quantity: 8 },
+//        { name: "rare candy", quantity: 99 },
+//      ],
+//      difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+//  if (Pikachu) {
+//     game.party.push(Pikachu);
+//  }
+
+//    const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+//  if (Nidoking) {
+//    game.party.push(Nidoking)
+//  }
+
+//    const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+//  if (Jigglypuff) {
+//    game.party.push(Jigglypuff)
+//  }
+
+//    const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+//  if (Charizard) {
+//    game.party.push(Charizard) 
+//  }
+
+//  game.gyms.forEach(gym => {
+//    if (gym.difficulty < 3) {
+//        gym.completed = true;
+//    }
+//  });
+  
+
+// game.party.sort((a, b) => b.hp - a.hp);
+
+//  console.log(game.party)
+
+ /*
+Exercise 10
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 10 here:
+*/
+
+// const pokemon = require('./data.js')
+
+//  const game = {
+//      party: [],
+//      gyms: [
+//        { location: "Pewter City", completed: false, difficulty: 1 },
+//        { location: "Cerulean City", completed: false, difficulty: 2 },
+//        { location: "Vermilion City", completed: false, difficulty: 3 },
+//        { location: "Celadon City", completed: false, difficulty: 4 },
+//        { location: "Fuchsia City", completed: false, difficulty: 5 },
+//        { location: "Saffron City", completed: false, difficulty: 6 },
+//        { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//        { location: "Viridian City", completed: false, difficulty: 8 },
+//      ],
+//     items: [
+//        { name: "potion", quantity: 4 },
+//        { name: "pokeball", quantity: 8 },
+//        { name: "rare candy", quantity: 99 },
+//      ],
+//      difficulty: "medium"
+//   }
+
+//   const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+//  if (Pikachu) {
+//     game.party.push(Pikachu);
+//  }
+
+//    const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+//  if (Nidoking) {
+//    game.party.push(Nidoking)
+//  }
+
+//    const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+//  if (Jigglypuff) {
+//    game.party.push(Jigglypuff)
+//  }
+
+//    const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+//  if (Charizard) {
+//    game.party.push(Charizard) 
+//  }
+
+//  game.gyms.forEach(gym => {
+//    if (gym.difficulty < 3) {
+//        gym.completed = true;
+//    }
+//  });
+  
+//  console.log("Starter Pokémon:");
+//  const starterPokemon = [];
+ 
+//  pokemon.forEach(pokemon => {
+//    if (pokemon.starter) {
+//      starterPokemon.push(pokemon.name);
+//    }
+//  });
+ 
+//  console.log(starterPokemon);
+ 
+
+// game.party.sort((a, b) => b.hp - a.hp);
+
+//  console.log(game.party)
+
+/*
+Exercise 11
+1. Add a method called `catchPokemon` to the `game` object. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 11 here:
+*/
+
+const pokemon = require('./data.js')
+
+ const game = {
+     party: [],
+     gyms: [
+       { location: "Pewter City", completed: false, difficulty: 1 },
+       { location: "Cerulean City", completed: false, difficulty: 2 },
+       { location: "Vermilion City", completed: false, difficulty: 3 },
+       { location: "Celadon City", completed: false, difficulty: 4 },
+       { location: "Fuchsia City", completed: false, difficulty: 5 },
+       { location: "Saffron City", completed: false, difficulty: 6 },
+       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+       { location: "Viridian City", completed: false, difficulty: 8 },
+     ],
+    items: [
+       { name: "potion", quantity: 4 },
+       { name: "pokeball", quantity: 8 },
+       { name: "rare candy", quantity: 99 },
+     ],
+     difficulty: "medium"
+  }
+
+  const Pikachu = pokemon.find(pokemon => pokemon.name === "Pikachu");
+ if (Pikachu) {
+    game.party.push(Pikachu);
+ }
+
+   const Nidoking = pokemon.find(pokemon => pokemon.name === "Nidoking");
+ if (Nidoking) {
+   game.party.push(Nidoking)
+ }
+
+   const Jigglypuff = pokemon.find(pokemon => pokemon.name === "Jigglypuff");
+ if (Jigglypuff) {
+   game.party.push(Jigglypuff)
+ }
+
+   const Charizard = pokemon.find(pokemon => pokemon.name === "Charizard");
+ if (Charizard) {
+   game.party.push(Charizard) 
+ }
+
+ game.gyms.forEach(gym => {
+   if (gym.difficulty < 3) {
+       gym.completed = true;
+   }
+ });
+  
+ console.log("Starter Pokémon:");
+ const starterPokemon = [];
+ 
+ pokemon.forEach(pokemon => {
+   if (pokemon.starter) {
+     starterPokemon.push(pokemon.name);
+   }
+ });
+ 
+ console.log(starterPokemon);
+
+ 
